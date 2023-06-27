e10f7663cfeb3d3676c67b4f1babe04a1bd01a40
Jeremy <limpinjeremy@gmail.com>
2023-06-27 13:25:01 +0800

    updating loadmore button

--- main.js
+++ main.js
@@ -53,20 +53,40 @@ darkModeIcon.onclick = () => {
   document.body.classList.toggle("dark-mode");
 };
 // loadmore
+// let loadMoreBtn = document.querySelector("#load-more");
+// let currentItem = 3;
+
+// loadMoreBtn.onclick = () => {
+//   let boxes = [...document.querySelectorAll(".container .card")];
+//   for (var i = currentItem; i < currentItem + 3; i++) {
+//     boxes[i].style.display = "inline-block";
+//   }
+//   currentItem += 3;
+
+//   if (currentItem >= boxes.length) {
+//     loadMoreBtn.style.display = "inline-block";
+//   }
+
+// };
 let loadMoreBtn = document.querySelector("#load-more");
-let currentItem = 3;
+let currentItems = 3;
 
-loadMoreBtn.onclick = () => {
-  let boxes = [...document.querySelectorAll(".container .card")];
-  for (var i = currentItem; i < currentItem + 3; i++) {
-    boxes[i].style.display = "inline-block";
+loadMoreBtn.addEventListener("click", (e) => {
+  const elementList = [...document.querySelectorAll(".container .card")];
+  e.target.classList.add("show-loader");
+  for (let i = currentItems; i < currentItems +3; i++) {
+    setTimeout( function(){
+      e.target.classList.remove('show-loader');
+      if (elementList[i]){
+        elementList[i].style.display = "inline-block";
+      }
+    })
   }
-  currentItem += 3;
+  currentItems += 3;
 
-  if (currentItem >= boxes.length) {
-    loadMoreBtn.style.display = "inline-block";
-  }
-};
+  if(currentItems >=elementList.length)
+  event.target.classList.add('loaded')
+});
 
 ScrollReveal({
   reset: true,
