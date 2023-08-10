71a02d59cb92238111dd7cda9417110fb39adacc
Jheremei Arciaga <limpinjeremy@gmail.com>
2023-08-10 20:11:35 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.addEventListener(
     window.removeEventListener("load", load, false);
     setTimeout(function () {
       loader.style.display = "none";
-    }, 1700);
+    }, 1300);
   },
   false
 );
