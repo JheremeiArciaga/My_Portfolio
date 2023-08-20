f354c09f1f84180c2121f3373215810fb1be7524
Jheremei <limpinjeremy@gmail.com>
2023-08-20 19:52:21 +0800

    optimize

--- main.js
+++ main.js
@@ -21,27 +21,27 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
+window.onscroll = () => {
+  const top = window.scrollY;
+  const offset = 150;
+  
+  sections.forEach((sec) => {
+    const secTop = sec.offsetTop;
+    const secHeight = sec.offsetHeight;
+    const id = sec.getAttribute("id");
+
+    if (top >= secTop - offset && top < secTop + secHeight - offset) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
       });
+      document.querySelector(`header nav a[href*="${id}"]`).classList.add("active");
     }
   });
-  // sticky navbar
-  let header = document.querySelector(".header");
-  header.classList.toggle("sticky", window.scrollY > 100);
 
-  // remove icon menu
+  const header = document.querySelector(".header");
+  header.classList.toggle("sticky", top > 100);
+
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
